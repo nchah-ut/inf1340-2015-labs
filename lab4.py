1d1e09681217559e86afb6f9e54fbd2411a80011
Niel (iSchool) <niel.chah@mail.utoronto.ca>
2015-11-19 11:36:00 -0500

    Lab 4 done

--- lab4.py
+++ lab4.py
@@ -18,11 +18,42 @@ __license__ = "MIT License"
 # Rewrite this code to use global constants, local variables and functions
 # Output the text to a file instead of printing it
 
+# global GST, PST, purchase
+PST = 0.05
+GST = 0.025
+test_item = 100
+
+def get_gst(purchase):
+    new_val = purchase * GST
+    return new_val
+
+
+def get_pst(purchase):
+    new_val = purchase * PST
+    return new_val
+
+
+# def log_file(data):
+#     with open("log.txt", "a") as write_file:
+#         write_file.write(data)
+
 
 def bill_of_sale(purchase):
 
-    print ("Amount of purchase: {0:.2f}".format(purchase))
-    print ("Provincial tax: {0:.2f}".format(purchase * .05))
-    print ("Federal tax: {0:.2f}".format(purchase * .025))
-    print ("Total tax: {0:.2f}".format(purchase * .075))
-    print ("Total sale: {0:.2f}".format(purchase * 1.075))
+    # Calculate GST, PST tax values separately; add to original purchase amount
+    gst_tax = get_gst(purchase)
+    pst_tax = get_pst(purchase)
+    total_tax = gst_tax + pst_tax
+    taxed_purchase = purchase + total_tax
+
+    with open("log.txt", "a") as log_file:
+        log_file.write("Amount of purchase: {0:.2f} \n".format(purchase))
+        log_file.write("Provincial tax: {0:.2f} \n".format(pst_tax))
+        log_file.write("Federal tax: {0:.2f} \n".format(gst_tax))
+        log_file.write("Total tax: {0:.2f} \n".format(total_tax))
+        log_file.write("Total sale: {0:.2f} \n".format(taxed_purchase))
+
+    return taxed_purchase
+
+
+bill_of_sale(test_item)

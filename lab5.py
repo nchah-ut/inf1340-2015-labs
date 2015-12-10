b3041c5379272c22d898d31487bd533f1665ac31
Niel (iSchool) <niel.chah@mail.utoronto.ca>
2015-12-10 11:18:55 -0500

    lab5 done

--- lab5.py
+++ lab5.py
@@ -26,8 +26,60 @@ class NameThatShapeGUI:
 
         # Create a main window
         self.main_window = Tkinter.Tk()
+        self.main_window.geometry('500x100')
+
+        # Create 3 frames to group widgets
+        self.top_frame = Tkinter.Frame(self.main_window)
+        self.middle_frame = Tkinter.Frame(self.main_window)
+        self.bottom_frame = Tkinter.Frame(self.main_window)
+
+        # Create widgets for the top frame
+        self.prompt_label = Tkinter.Label(self.top_frame, text="Enter the number of sides in the shape:")
+        self.side_entry = Tkinter.Entry(self.top_frame, width=10)
+
+        # Pack 
+        #self.label = Tkinter.Label(self.main_window, text="Enter the number of sides in the shape:")
+        self.prompt_label.pack(side='left')
+        self.side_entry.pack(side='left')
+
+        # Create widgets for the middle frame
+        self.descr_label = Tkinter.Label(self.middle_frame, text="Name of Shape :")
+        self.value = Tkinter.StringVar()
+        self.value.set("")
+        self.shape_name = Tkinter.Label(self.middle_frame, textvariable=self.value)
+
+        # Pack the middle frame's widgets
+        self.descr_label.pack(side="left")
+        self.shape_name.pack(side="left")
+
+        # Create widgets for bottom frame
+        self.conv_button = Tkinter.Button(self.bottom_frame, text="NAME", command=self.convert)
+        self.quit_button = Tkinter.Button(self.bottom_frame, text='QUIT', command=self.main_window.destroy)
+
+        # Pack the bottom buttons
+        self.conv_button.pack(side="left")
+        self.quit_button.pack(side="left")
+
+        # Pack the frames
+        self.top_frame.pack()
+        self.middle_frame.pack()
+        self.bottom_frame.pack()
 
         Tkinter.mainloop()
+
+    def convert(self):
+        # Display
+        self.value.set(self.side_entry.get())
+
+        #self.value = int(self.value)
+
+        try:
+            sides = str(self.side_entry.get())
+            self.value.set(name_that_shape(sides))
+            self.shape_name = Tkinter.Label(self.middle_frame, textvariable=self.value)
+        except ValueError:
+            tkMessageBox.showinfo("Response", "Error!!")
+
 
 
 # Instantiating the object
